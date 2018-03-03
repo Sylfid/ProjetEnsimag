0c76303d06faa0b4269453f969e24cb54ec44298
Anatole GALLOUET <Anatole.Gallouet@grenoble-inp.org>
2018-03-03 13:34:07 +0100

    test

---
+++ TP1_lavainnr_gallouea/src/Dvector.h
@@ -0,0 +1,21 @@
+#include <string>
+
+
+class Dvector{
+    private:
+        double* composante;
+        int taille;
+    public:
+        Dvector();
+        Dvector(int taille2);
+        Dvector(Dvector const& copie);
+        Dvector(int taille2, double initialisation);
+        Dvector(std::string file);
+        ~Dvector();
+        void display(std::ostream& str);
+        void setVector(int i, double newvaleur);
+        void addValeur(double newValeur);
+        void fillRandomly();
+        int size();
+};
+
