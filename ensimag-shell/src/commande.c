64ef5bd94f5718689f9174b4c00303d8ae9b9f83
Lavainne Remy <remy.lavainne@grenoble-inp.org>
2018-03-20 13:36:17 +0100

    aaaaa

--- ensimag-shell/src/commande.c
+++ ensimag-shell/src/commande.c
@@ -2,19 +2,15 @@
 
 void execcmd(struct cmdline *cmd){
         pid_t pid ;
-        int status;
         switch (pid=fork()){
             case -1:
                 perror("fork");
             case 0:
                 execvp(*cmd->seq[0],*cmd->seq);
-                if(strncmp(*cmd->seq[0],"jobs",4)){
-                    assert(0);
-                }
-                //assert(0);
+                assert(0);
             default:
                 if(cmd->bg == 0){
-                    waitpid(0,&status,0);
+                   waitpid(0,NULL,0);
                 }
                 break;
         }
