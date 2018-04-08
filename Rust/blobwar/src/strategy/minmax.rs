271ce153e196d2e6647a1541bb935c1b527d2483
Rémy Lavainne <remy.lavainne@ensimag.grenoble-inp.fr>
2018-04-08 12:12:44 +0200

    min-max

--- Rust/blobwar/src/strategy/minmax.rs
+++ Rust/blobwar/src/strategy/minmax.rs
@@ -10,10 +10,10 @@ pub struct MinMax(pub u8);
 impl Strategy for MinMax {
     fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
        let mut m = None;
-       let mut val;
-       let mut max = -state.value();
+       let mut val:i8;
+       let mut max = compute_best_move(self.0, &state.skip_play(),state.current_player);
        for mov in state.movements(){
-           val = compute_best_move(self.0, state.play(&mov));
+           val = compute_best_move(self.0, &state.play(&mov), state.current_player);
            if val > max{
                max = val;
                m = Some(mov);
@@ -30,22 +30,52 @@ impl fmt::Display for MinMax {
     }
 }
 
-fn compute_best_move(depth: i8, state: &Configuration) -> i8 {
-   if (depth as i8) == 0{
-       state.value()
+fn compute_best_move(depth: u8, state: &Configuration, player: bool) -> i8 {
+    let mut a:i8;
+    let mut b:i8;
+   if(state.current_player){
+       a=state.value();
    }
-   let mut best_move:i8= -state.value();
-   let mut other_move:i8=0;
-   let mut &Configuration newState;
-   for mov in state.movements(){
-       newState = state.play(&mov);
-       other_move=compute_best_move(depth-1,newState);
-       if best_move < other_move{ 
-           best_move=other_move;
+   else{
+       a=-state.value();
+   }
+   if (player) {
+       a = a*-1;
+   }
+   if (depth as u8) == 0{
+       a
+   }
+   else{
+       let mut flag=false;
+       let mut other_move:i8;
+       let mut new_state;
+       new_state=state.skip_play();
+       let mut best_move:i8 = compute_best_move(depth-1,&new_state,player); 
+       for mov in state.movements(){
+           flag=true;
+           new_state = state.play(&mov);
+           other_move=compute_best_move(depth-1, &new_state, player);
+           if(!state.current_player ^ player){
+               if best_move < other_move{ 
+                   best_move=other_move;
+               }
+           }
+           else{
+               if best_move > other_move{ 
+                   best_move=other_move;
+               }
+           }
+       }
+       if (!flag){
+           a
+       }
+       else{
+           best_move
        }
    }
-   best_move 
 }
+
+
 /// Anytime min max algorithm.
 /// Any time algorithms will compute until a deadline is hit and the process is killed.
 /// They are therefore run in another process and communicate through shared memory.
